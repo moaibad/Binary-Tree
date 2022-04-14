d2aeef7d7f000e91e0b7a1c04a9ccdbad6a80888
mantoZONE <fathulibad.412@gmail.com>
2022-04-14 14:44:54 +0700

    first commit

---
+++ tree.c
@@ -0,0 +1,12 @@
+/*
+    File         	: tree.c
+    Description    	: Body ADT Binary Tree
+    Author    		: Kelompok A6
+    Date/Version   	: 8 April 2022/1.0
+    Compiler        : TDM-GCC 4.9.2
+*/
+
+#include <malloc.h>
+#include "tree.h"
+
+
---
+++ tree.h
@@ -0,0 +1,39 @@
+/*
+    File         	: tree.c
+    Description    	: Header ADT Binary Tree
+    Author    		: Kelompok A6
+    Date/Version   	: 8 April 2022/1.0
+    Compiler        : TDM-GCC 4.9.2
+*/
+
+#ifndef tree_H
+#define tree_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "boolean.h"
+#include "tree.h"
+#define Nil NULL
+#define Nama(P) (P)->nama
+#define Id(P) (P)->id
+#define Left(P) (P)->Left
+#define Right(P) (P)->Right
+#define Root(T) (T).root
+
+typedef struct tElmTree *address;
+
+typedef struct tElmTree{
+	int id;
+	char nama[20];
+	address left;
+	address right;
+}ElmTree;
+
+
+typedef struct{
+	nbAddr root;
+}Tree;
+
+
+
+#endif
